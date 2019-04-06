935350d75b5aff17424ea6aaf0b595082fd5db5f
Robert Burek <robertburek@wp.pl>
2019-04-06 19:40:51 +0200

    ENCJE - modyfikacja klasy student na JPA/HIBERNATE

--- src/main/java/jpa/domain/Student.java
+++ src/main/java/jpa/domain/Student.java
@@ -1,13 +1,34 @@
 package jpa.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
 /**
  * Created by Robert Burek
  */
+@Entity//(name = "uczniowie") dodatkowe znaczniki
 public class Student {
 
+    @Id //symbol przy unikatowym polu
     private int id;
+
+//    @Column(name="firstname", nullable = false) - dodatkowe znaczniki
     private String imie;
+
+//    @Column(name="surname") - dodatkowe znaczniki
     private String nazwisko;
+
+//    @Column(unique = true)  - dodatkowe znaczniki
+    private String telephon;
+
+//    @Transient  //  to pole ma nie być mapowane do bazy danych  - dodatkowe znaczniki
+    private String pesel;
+
+    // hibernate wymaga pustego konstruktora ale może on być prywatny czyli taki którego nie użtwa się w prost
+    private Student() {
+    }
 
     public Student(int id, String imie, String nazwisko) {
         this.id = id;
