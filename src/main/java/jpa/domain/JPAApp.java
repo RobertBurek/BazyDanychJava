1cc8bc3acdb76dcfc8be8d37e920d55cb52f8b65
Robert Burek <robertburek@wp.pl>
2019-04-09 17:22:49 +0200

    Pytania JPQL poprzez entityManager.createQuery oraz getResuktList

--- src/main/java/jpa/domain/JPAApp.java
+++ src/main/java/jpa/domain/JPAApp.java
@@ -26,130 +26,152 @@ public class JPAApp {
     public static void main(String[] args) {
 
 
-        //CRUD
-        //Create
-        creatStudent();
-//        System.out.println("-------------------utworzyłęm tabelę w bazie-----------------------");
-//        readStudents();
-//        System.out.println("---------------------odczytałem elementy tablicy z bazy---------------------------------");
-//        Student ktos = readStudent(6);
-//        System.out.println(ktos);
-//        System.out.println("--------------------odczytałem dane nr=6 studenta z bazy--i zapisałe do objektu ktos----");
-        entityManager.getTransaction().begin();
-        Indeks indeks = entityManager.merge(new Indeks("123"));
-        Indeks indeks0 = entityManager.merge(new Indeks( "000"));
-        Indeks indeks1 = entityManager.merge(new Indeks( "111"));
-        Indeks indeks2 = entityManager.merge(new Indeks( "222"));
-//        Indeks indeks3 = entityManager.merge(new Indeks( "333"));
-//        Indeks indeks4 = entityManager.merge(new Indeks( "444"));
-//        Indeks indeks5 = entityManager.merge(new Indeks( "555"));
-        entityManager.getTransaction().commit();
-//        System.out.println(indeks);
-////        System.out.println(indeks0);
-////        System.out.println(indeks1);
-////        System.out.println(indeks2);
-////        System.out.println(indeks3);
-////        System.out.println(indeks4);
-////        System.out.println(indeks5);
-//        System.out.println("--------------------stworzyłem indeks nr6 o numerze 123--------------------------------");
-//        ktos.setIndeks(indeks);
-//        System.out.println(ktos);
-//        System.out.println("----------ustawiłem indeks do obiektu ktos------------------------------------------");
-//        Address address = new Address("Domaniewska", "25", "02-300", "Warszawa");
-//        System.out.println(address);
-//        System.out.println("--------------------stworzyłem objekt address wszystkie dane adresowe------------------");
-//        ktos.setAddress(address);
-//        System.out.println(ktos);
-//        System.out.println("-------------ustawiłem address na objelcie ktos--------------------------------");
-//        ktos = entityManager.merge(ktos);
-//        System.out.println(ktos);
-//        System.out.println("--------------------wykonałem marge na objekcie ktos (nadpisałem w bazie)-----------");
-//        System.out.println(readStudent(6));
-//        System.out.println("--------------------wypisałem studenta nr6 na ekran dane wziąłem z bazy-----------------");
-//        //System.out.println(ktos);
-//        //Read
-//        //   readStudents();
-//        //Update
-//        updateStudent(3, "", "", "654321", "");
-//        System.out.println("--------------------zrobiłem update na nr3 student telefon 654321 bez merga ---------");
-//        //Delete
-//        //deleteStudent(5);
+//        //CRUD
+//        //Create
+//        creatStudent();
+////        System.out.println("-------------------utworzyłęm tabelę w bazie-----------------------");
+////        readStudents();
+////        System.out.println("---------------------odczytałem elementy tablicy z bazy---------------------------------");
+////        Student ktos = readStudent(6);
+////        System.out.println(ktos);
+////        System.out.println("--------------------odczytałem dane nr=6 studenta z bazy--i zapisałe do objektu ktos----");
+//        entityManager.getTransaction().begin();
+//        Indeks indeks = entityManager.merge(new Indeks("123"));
+//        Indeks indeks0 = entityManager.merge(new Indeks( "000"));
+//        Indeks indeks1 = entityManager.merge(new Indeks( "111"));
+//        Indeks indeks2 = entityManager.merge(new Indeks( "222"));
+////        Indeks indeks3 = entityManager.merge(new Indeks( "333"));
+////        Indeks indeks4 = entityManager.merge(new Indeks( "444"));
+////        Indeks indeks5 = entityManager.merge(new Indeks( "555"));
+//        entityManager.getTransaction().commit();
+////        System.out.println(indeks);
+//////        System.out.println(indeks0);
+//////        System.out.println(indeks1);
+//////        System.out.println(indeks2);
+//////        System.out.println(indeks3);
+//////        System.out.println(indeks4);
+//////        System.out.println(indeks5);
+////        System.out.println("--------------------stworzyłem indeks nr6 o numerze 123--------------------------------");
+////        ktos.setIndeks(indeks);
+////        System.out.println(ktos);
+////        System.out.println("----------ustawiłem indeks do obiektu ktos------------------------------------------");
+////        Address address = new Address("Domaniewska", "25", "02-300", "Warszawa");
+////        System.out.println(address);
+////        System.out.println("--------------------stworzyłem objekt address wszystkie dane adresowe------------------");
+////        ktos.setAddress(address);
+////        System.out.println(ktos);
+////        System.out.println("-------------ustawiłem address na objelcie ktos--------------------------------");
+////        ktos = entityManager.merge(ktos);
+////        System.out.println(ktos);
+////        System.out.println("--------------------wykonałem marge na objekcie ktos (nadpisałem w bazie)-----------");
+////        System.out.println(readStudent(6));
+////        System.out.println("--------------------wypisałem studenta nr6 na ekran dane wziąłem z bazy-----------------");
+////        //System.out.println(ktos);
+////        //Read
+////        //   readStudents();
+////        //Update
+////        updateStudent(3, "", "", "654321", "");
+////        System.out.println("--------------------zrobiłem update na nr3 student telefon 654321 bez merga ---------");
+////        //Delete
+////        //deleteStudent(5);
+////
+////        readStudents();
+////        System.out.println("--------------------wypisałem wszystkich studentów na ekran dane wziąłem z bazy ---------");
+////        readIndeks();
+////        System.out.println("--------------------wypisałem wszystkie indeksy na ekran dane wziąłem z bazy ---------");
+////        Student innyktos = readStudent(1);
+////        Indeks innyIndeks = entityManager.merge(new Indeks("000"));
+////        innyktos.setIndeks(innyIndeks);
+////        innyIndeks.setOwner(innyktos);
+////
+////        entityManager.merge(innyIndeks);
+////        entityManager.merge(innyktos);
+////
+////        Indeks indx = entityManager.find(Indeks.class,1);
+////        System.out.println(indx);
+////        System.out.println(readStudent(1));
+////
+////        Indeks mergeIndeks = entityManager.merge(indeks);
+////        Indeks idxx = entityManager.find(Indeks.class,mergeIndeks.getId());
+////        System.out.println(idxx);
 //
+//        entityManager.getTransaction().begin();
+//        Student monika = entityManager.merge(new Student("Monika","Nowakowska", "343434",
+//                "89120206324","898"));
+//        entityManager.getTransaction().commit();
+//        System.out.println(monika);
+//        System.out.println("--------------");
 //        readStudents();
-//        System.out.println("--------------------wypisałem wszystkich studentów na ekran dane wziąłem z bazy ---------");
+//        System.out.println("-------------------------");
 //        readIndeks();
-//        System.out.println("--------------------wypisałem wszystkie indeksy na ekran dane wziąłem z bazy ---------");
-//        Student innyktos = readStudent(1);
-//        Indeks innyIndeks = entityManager.merge(new Indeks("000"));
-//        innyktos.setIndeks(innyIndeks);
-//        innyIndeks.setOwner(innyktos);
 //
-//        entityManager.merge(innyIndeks);
-//        entityManager.merge(innyktos);
+//        entityManager.getTransaction().begin();
+//        University umk = entityManager.merge(new University("UMK"));
+//        umk.addStudent(monika);
+//        monika.setUniversity(umk);
+//        entityManager.merge(umk);
+//        entityManager.merge(monika);
 //
-//        Indeks indx = entityManager.find(Indeks.class,1);
-//        System.out.println(indx);
-//        System.out.println(readStudent(1));
+//        readStudent(7);
+//        Classes geografia = new Classes("GEOGRAFIA");
+//        Classes matematyka = new Classes("MATEMATYKA");
+//        matematyka.addStudent(monika);
+//        matematyka.addStudent(readStudent(3));
+//        geografia.addStudent(monika);
+//        geografia.addStudent(readStudent(4));
+//        entityManager.merge(geografia);
+//        entityManager.merge(matematyka);
 //
-//        Indeks mergeIndeks = entityManager.merge(indeks);
-//        Indeks idxx = entityManager.find(Indeks.class,mergeIndeks.getId());
-//        System.out.println(idxx);
+//
+//        System.out.println(geografia);
+//        System.out.println(matematyka);
+//        Student nowy = new Student("Imie","Nazwisko");
+//        nowy.addClasses(geografia);
+//        entityManager.persist(nowy);
+//        entityManager.getTransaction().commit();
+//        System.out.println("___NOWY_____");
+//        System.out.println(nowy.toString(true));
+//        System.out.println("___NOWY_____");
+////        monika.addClasses(geografia);
+////        readStudent(4).addClasses(geografia);
+//        System.out.println(monika.toString(true));
+//        System.out.println(readStudent(4).toString(true));
+////        for (int i=1; i<4 ;++i) umk.addStudent(readStudent(i));
+////        entityManager.getTransaction().commit();
+////        System.out.println(umk);
+//        //umk.toString();
+//        readStudents();
+
+        createData();
+
+        entityManager.createQuery("SELECT s FROM Student s WHERE s.imie LIKE '%ob%'").getResultList().forEach(System.out::println);
+
+//        List<Indeks> resultList = entityManager.createQuery("SELECT s.indeks FROM Student s", Indeks.class).getResultList();
+
+
+    }
+
+    private static void createData(){
 
         entityManager.getTransaction().begin();
         Student monika = entityManager.merge(new Student("Monika","Nowakowska", "343434",
-                "89120206324","898"));
+                "89120206324","111"));
+        Student robert = entityManager.merge(new Student("Robert","Mańkowski", "111111",
+                "85122456856","222"));
         entityManager.getTransaction().commit();
-        System.out.println(monika);
-        System.out.println("--------------");
-        readStudents();
-        System.out.println("-------------------------");
-        readIndeks();
-
-        entityManager.getTransaction().begin();
-        University umk = entityManager.merge(new University("UMK"));
-        umk.addStudent(monika);
-        monika.setUniversity(umk);
-        entityManager.merge(umk);
-        entityManager.merge(monika);
-
-        readStudent(7);
-        Classes geografia = new Classes("GEOGRAFIA");
-        Classes matematyka = new Classes("MATEMATYKA");
-        matematyka.addStudent(monika);
-        matematyka.addStudent(readStudent(3));
-        geografia.addStudent(monika);
-        geografia.addStudent(readStudent(4));
-        entityManager.merge(geografia);
-        entityManager.merge(matematyka);
-
-
-        System.out.println(geografia);
-        System.out.println(matematyka);
-        Student nowy = new Student("Imie","Nazwisko");
-        nowy.addClasses(geografia);
-        entityManager.persist(nowy);
-        entityManager.getTransaction().commit();
-        System.out.println("___NOWY_____");
-        System.out.println(nowy.toString(true));
-        System.out.println("___NOWY_____");
-//        monika.addClasses(geografia);
-//        readStudent(4).addClasses(geografia);
-        System.out.println(monika.toString(true));
-        System.out.println(readStudent(4).toString(true));
-//        for (int i=1; i<4 ;++i) umk.addStudent(readStudent(i));
-//        entityManager.getTransaction().commit();
-//        System.out.println(umk);
-        //umk.toString();
-        readStudents();
-
-
-
+//        System.out.println(monika);
+//        System.out.println("---------------------------------------------------------------------------------------");
+////        readStudents();
+//       // readStudent(0);
+//
+//        readStudent(2);
+//        System.out.println("---------------------------------------------------------------------------------------");
 
     }
 
     private static Student readStudent(int nr) {
         //entityManager.getTransaction().begin();
-        Student student = entityManager.find(Student.class, nr);
+        Student student = entityManager.find(Student.class,nr);
         //entityManager.getTransaction().commit();
         return student;
     }
